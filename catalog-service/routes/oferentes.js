9c7e84e1b2d8c402db727bb7eb2f9080cf6510a6
Joshua Luna <transwfc@gmail.com>
2026-04-15 00:14:29 -0600

    whatever

--- catalog-service/routes/oferentes.js
+++ catalog-service/routes/oferentes.js
@@ -5,8 +5,8 @@ const { verifyToken, verifyAdmin } = require('../middleware/auth');
 
 router.post('/', verifyToken, oferenteController.crearOferente);
 router.get('/', oferenteController.obtenerOferentes);
-router.get('/:id', oferenteController.obtenerOferentePorId);
 router.get('/usuario/:userId', oferenteController.obtenerOferentePorUsuario);
+router.get('/:id', oferenteController.obtenerOferentePorId);
 router.put('/:id', verifyToken, oferenteController.actualizarOferente);
 router.delete('/:id', verifyToken, verifyAdmin, oferenteController.eliminarOferente);
 router.patch('/:id/estado', verifyToken, verifyAdmin, oferenteController.actualizarEstadoOferente);
